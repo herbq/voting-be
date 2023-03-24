e653fa0188f2426ea3251d771a284b43e9575fdf
herbq <81512633+stqven@users.noreply.github.com>
2023-03-24 23:55:42 +0200

    test

--- index.ts
+++ index.ts
@@ -1,35 +1,30 @@
 import express from "express";
-import mongoose from 'mongoose';
-import { generateKeys } from "./src/services/subtle/rsa.service";
+// import mongoose from 'mongoose';
+// import { generateKeys } from "./src/services/subtle/rsa.service";
 
-import userRouter from "./src/routes/user.route";
-import electionRouter from "./src/routes/election.route";
+// import userRouter from "./src/routes/user.route";
+// import electionRouter from "./src/routes/election.route";
 
 const app = express();
-var cors = require('cors');
 
-
-app.use(cors())
-app.use(express.json());
-
-
-app.use('/user', userRouter);
-app.use('/election', electionRouter);
+app.use('/api', (req, res, next) => {
+  res.json({"result": "success"});
+});
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://127.0.0.1:${port}`);
-  dbConnect();
+  // dbConnect();
 });
 
-const dbConnect = () => {
-  console.log("connecting to db...");
-  mongoose.connect("mongodb://127.0.0.1:27017/voting")
-    .then(async () => {
-      console.log(`🤗 [server]: Connected to MongoDB`);
-    })
-    .catch((err) => {
-      console.log(`🤨 [server]: Failed to connect to mongodb ${err}`);
-    });
-}
+// const dbConnect = () => {
+//   console.log("connecting to db...");
+//   mongoose.connect("mongodb://127.0.0.1:27017/voting")
+//     .then(async () => {
+//       console.log(`🤗 [server]: Connected to MongoDB`);
+//     })
+//     .catch((err) => {
+//       console.log(`🤨 [server]: Failed to connect to mongodb ${err}`);
+//     });
+// }
