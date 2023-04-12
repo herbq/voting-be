3f22ffd36aebb67919104159c5b34a7e7bd5b171
herbq <81512633+stqven@users.noreply.github.com>
2023-04-12 18:42:19 +0300

    qwewqe

--- index.ts
+++ index.ts
@@ -1,37 +1,60 @@
 import express from "express";
-import mongoose from 'mongoose';
-import { generateKeys } from "./src/services/subtle/rsa.service";
-
-import userRouter from "./src/routes/user.route";
-import electionRouter from "./src/routes/election.route";
 import cors from 'cors';
+import botRoute from './src/routes/bot.route';
+import profileRoute from './src/routes/profile.route';
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 
-app.use('/api', (req, res, next) => {
-  res.json({"resultwww45": "success"});
-});
-
-app.use(`/user`, userRouter);
-app.use(`/election`, electionRouter);
 
 const port = process.env.PORT || 8000;
 
+app.use(`/bot`, botRoute);
+app.use(`/profile`, profileRoute);
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://127.0.0.1:${port}`);
+  try {
+    test();
+  } catch {
+    console.log(`Error while running test() function.`)
+  }
   dbConnect();
 });
 
 const dbConnect = () => {
-  console.log("connecting to db...");
-  mongoose.connect("mongodb://127.0.0.1:27017/voting")
+  console.log("🤖 [server]: Connecting to db...");
+  mongoose.connect("mongodb://127.0.0.1:27017/chatbot")
     .then(async () => {
-      console.log(`🤗 [server]: Connected to MongoDB`);
+      console.log(`🤖 [server]: Connected to MongoDB`);
     })
     .catch((err) => {
-      console.log(`🤨 [server]: Failed to connect to mongodb ${err}`);
+      console.log(`🤖 [server]: Failed to connect to mongodb ${err}`);
     });
+}
+
+const test = async () => {
+  const li_at = `AQEDAUJdX7sAzGX2AAABh1idEO0AAAGHfKmU7VYAB5YkSh6SrlW9PCTqYY9KcM2THJxRqM1AK9Py3f1cTXO77ytCwibCbi-K-vMvTvSiMbP-GcJLSwUmTVtJwUzL99ChJfvMvws8YY-heW64ROxd8AcN`;
+  try {
+    // const response = await sendMessage(
+    //   li_at,
+    //   await getPublicID(`https://www.linkedin.com/in/omar-undefined-8a3a69270/`, li_at),
+    //   await getPublicID(`https://www.linkedin.com/in/omar-herbawi-6468aa21a/`, li_at));
+    // if (response != null) {
+    //   // console.log({ data: response.data, status: response.status });
+    // }
+
+    // const publicIDResponse = await getPublicID(`https://www.linkedin.com/in/omar-herbawi-6468aa21a/`, li_at);
+    // console.log({ publicIDResponse });
+
+    // console.log(`conversationResponse`);
+    // const conversationResponse = await getConversationData(li_at);
+    // console.log({ data: conversationResponse.data, status: conversationResponse.status });
+
+  } catch {
+    console.log(`An error occurred while sending the message`)
+  }
 }
--- src/handlers/mysql_error.handler.ts
+++
@@ -1,7 +0,0 @@
-
-export class MySQLError extends Error {
-
-  constructor(message: string) {
-    super(message);
-  }
-}
